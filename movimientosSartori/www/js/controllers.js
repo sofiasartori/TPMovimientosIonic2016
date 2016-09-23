04fc5180685d0851190e5a5594bf079b37fd1f20
Sofia Sartori <Sofia Sartori>
2016-09-23 17:56:19 -0300

    Agregado de icono y splash screen. Prueba del plugin media

--- movimientosSartori/www/js/controllers.js
+++ movimientosSartori/www/js/controllers.js
@@ -1,8 +1,8 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats, $cordovaDeviceMotion, $cordovaMedia) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,7 +10,19 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  var src= "/sounds/audio.mp3"; 
+  var media = $cordovaMedia.newMedia(src);
+  
+  $scope.grabar = function(){   
+    media.startRecord();
+  }
 
+  $scope.guardarJson=function(){
+    media.stopRecord();
+    media.play();
+  }
+
+  
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
